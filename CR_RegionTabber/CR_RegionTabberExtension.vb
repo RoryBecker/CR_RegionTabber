1e6387801311b2d9861159f333c36d0126121218
Rory Becker <RoryBecker@gmail.com>
2014-12-01 22:50:55 +0000

    Initial Load.

---
+++ CR_RegionTabber/CR_RegionTabberExtension.vb
@@ -0,0 +1,7 @@
+Imports System.ComponentModel.Composition
+Imports DevExpress.CodeRush.Common
+
+<Export(GetType(IVsixPluginExtension))> _
+Public Class CR_RegionTabberExtension
+    Implements IVsixPluginExtension
+End Class
